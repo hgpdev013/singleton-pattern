de1e6179daef758b7b95923f62f967387198efe7
Hugo <hugohenrique7@hotmail.com.br>
2023-09-20 21:53:51 -0300

    fix: instance return

--- src/Fila.java
+++ src/Fila.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Fila {
+public class Fila {
+    private static Fila instance;
+    private Fila(){
+
+    }
+    private void ImprimeDocumento(){
+
+    }
+    private void RemoveDocumento(){
+
+    }
+    private void RemoveTodosDocumentos(){
+
+    }
+
+    public static Fila getInstance(){
+        if (instance == null) {
+            instance = new Fila();
+        }
+        return instance;
+    }
 }
